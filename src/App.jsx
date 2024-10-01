265c4196e2c4c4389acdc51e91d023eb027be90c
Aneel Grewal <aneel.grewal@hotmail.com>
2024-10-01 22:26:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./header/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
---
+++ src/header/Header.jsx
@@ -0,0 +1,28 @@
+const Header = () => {
+  return (
+    <>
+      <div className="bg-yellow-200 w-full h-14 flex justify-between px-5 items-center">
+        <img
+          src="https://e7.pngegg.com/pngimages/427/304/png-clipart-pizza-sticker-cartoon-drawing-pizza-food-fruit-thumbnail.png"
+          className="w-14 h-12"
+        />
+        <ul className="flex items-center gap-5 text-2xl font-bold text-orange-700">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Contact</li>
+          <li className="cursor-pointer">Cart</li>
+        </ul>
+        <div className="flex">
+          <button className="w-24 h-10 mx-5 bg-green-600 text-xl font-bold rounded-lg">
+            Login
+          </button>
+          <button className="w-24 h-10 bg-red-500 text-xl font-bold rounded-lg">
+            Logout
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
