bc1dd8eafbfe119e63f165277574ad527aa6f4de
Aneel Grewal <aneel.grewal@hotmail.com>
2024-10-15 23:39:38 +0200

    Cart component updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,46 @@
+import { useSelector } from "react-redux";
+import ItemLayout from "./ItemLayout";
+
 const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.itemsadded);
   return (
     <div className="w-full h-[580px] px-2">
-      <h2 className="font-bold text-xl text-gray-700">Cart page</h2>
+      <div className="w-auto flex justify-center my-10">
+        <table className="shadow-lg bg-white border-collapse text-center">
+          <thead>
+            <tr>
+              <th className="bg-slate-200 border text-left px-8 py-4">
+                ITEM ID
+              </th>
+              <th className="bg-slate-200 border text-left px-8 py-4">SIZE</th>
+
+              <th className="bg-slate-200 border text-left px-8 py-4">
+                PRICE PER PIECE
+              </th>
+              <th className="bg-slate-200 border text-left px-8 py-4">
+                QUANTITY
+              </th>
+              <th className="bg-slate-200 border text-left px-8 py-4">
+                PRICE FOR ALL PIECES
+              </th>
+              <th className="bg-slate-200 border text-left px-8 py-4"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((i, index) => (
+              <ItemLayout {...i} key={index} />
+            ))}
+          </tbody>
+          <tfoot className="hover:bg-gray-50">
+            <tr>
+              <td className="border px-8 py-4" colSpan={4}>
+                Total amount to paid:
+              </td>
+              <td className="border px-8 py-4">72183</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 };
